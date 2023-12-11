39a19fd794211fdf7fc28ad1ed7e3ce651ed8206
Prince22 <112156035+Princebhan@users.noreply.github.com>
2023-12-11 22:21:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,55 +1,30 @@
 const connectTOMongo = require('./db');
-const express = require('express');
-const cors = require('cors');
-const http = require('http');
-const socketIo = require('socket.io');
-const path = require('path');
+const express = require('express')
+var cors=require('cors')
+var path = require('path')
 
 connectTOMongo();
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express()
+//const port = 5000
+const port = process.env.REACT_APP_PORT || 5000;
+app.use(express.json())
 
-app.use(express.json());
-app.use(cors());
 
-// Available Routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/notes', require('./routes/notes'));
+app.use(cors())
+app.use(express.json())
 
-// Serve static files
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+//availabel Routes
+
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
+
+/// static file start
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
-// Create an HTTP server
-const server = http.createServer(app);
-
-// Attach Socket.io to the HTTP server
-const io = socketIo(server, {
-  cors: {
-    origin: 'https://inotebook-6pk4.onrender.com', // Replace with your actual frontend app URL
-    credentials: true,
-  },
-});
-
-// WebSocket connection handling
-io.on('connection', (socket) => {
-  console.log('A user connected:', socket.id);
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
-  });
-
-  // Additional WebSocket logic goes here
-
-  // Example: Broadcasting a message to all connected clients
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-});
-
-// Listen on the specified port
-server.listen(port, () => {
-  console.log(`iNoteBook Backend listening on port http://localhost:${port}`);
-});
+app.listen(port, () => {
+  console.log(`iNoteBook Backend listening on port http://localhost:${port}`)
+})
